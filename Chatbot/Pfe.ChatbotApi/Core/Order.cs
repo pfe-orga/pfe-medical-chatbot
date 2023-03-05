6f6f797fa7b1fdc9bda201c98b14efac89d8307f
sfaxi melek <sfaxi.malek@outlook.fr>
2023-03-05 12:42:45 +0100

    feat : Connecting DataBase

---
+++ Chatbot/Pfe.ChatbotApi/Core/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pfe.ChatbotApi.Core
+{
+    [Table("order")]
+    public class Order
+    {
+        [Key, Required]
+        public int Id { get; set; }
+        public int prodfuct_Id { get; set; }
+        public virtual Product Product { get; set; }
+        public string name { get; set; } = string.Empty;
+        public string adress { get; set; } = string.Empty;
+        public string phone { get; set; } = string.Empty;
+
+
+    }
+}
---
+++ Chatbot/Pfe.ChatbotApi/Core/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pfe.ChatbotApi.Core
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key, Required]
+        public int Id { get; set; }
+        public int name { get; set; }
+        public int brand { get; set; }
+        public int size { get; set; }
+        public int price { get; set; }
+    }
+}
