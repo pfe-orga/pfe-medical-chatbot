945aef2fb3ecb9b7dd7e5a2502af7aa99c246df0
sfaxi melek <sfaxi.malek@outlook.fr>
2023-03-15 06:38:09 +0100

    Feat : Authentification using JWT

---
+++ Chatbot/Pfe.ChatbotApi/Controllers/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pfe.ChatbotApi.Services.Classes;
+
+namespace Pfe.ChatbotApi.Controllers
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    
+}
--- Chatbot/Pfe.ChatbotApi/Program.cs
+++ Chatbot/Pfe.ChatbotApi/Program.cs
@@ -3,16 +3,37 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Web;
-using Pfe.ChatbotApi.Core;
+using Microsoft.IdentityModel.Tokens;
+using Pfe.ChatbotApi.Controllers;
 using Pfe.ChatbotApi.Services.Classes;
-using Pfe.ChatbotApi.Services.Interfaces;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>
-    (options =>options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
-builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["SecureApi"],
+        ValidAudience = builder.Configuration["SecureApiUser"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["sz8eI7OdHBrjrIo8j9nTW/rQyO1OvY0pAQ2wDKQZw/0="]))
+    };
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnection")));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
@@ -31,7 +52,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
