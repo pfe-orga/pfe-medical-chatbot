f77e6a0b89f9b51485cec1ff82e607e85a614e4f
sfaxi melek <sfaxi.malek@outlook.fr>
2023-05-01 21:42:24 +0100

    Feat :Roles

---
+++ ChatbotApi/Pfe.ChatbotApi/Core/Roles.cs
@@ -0,0 +1,17 @@
+﻿namespace Pfe.ChatbotApi.Core
+{
+    public static class Roles
+    {
+        public const string Doctor = "Doctor";
+        public const string Admin = "Admin";
+        public const string Patient = "Patient";
+
+        public static IEnumerable<string> AllRoles()
+        {
+            yield return Doctor;
+            yield return Admin;
+            yield return Patient;
+        }
+    }
+
+}
