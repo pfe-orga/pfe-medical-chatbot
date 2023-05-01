a3d21146621f42509394f23c66acf6b15825551c
sfaxi melek <sfaxi.malek@outlook.fr>
2023-05-01 21:44:32 +0100

    feat :admin

--- ChatbotApi/Pfe.ChatbotApi/Services/Classes/AdminService.cs
+++ ChatbotApi/Pfe.ChatbotApi/Services/Classes/AdminService.cs
@@ -5,15 +5,15 @@ using System.Collections.Generic;
 
 namespace Pfe.ChatbotApi.Services.Classes
 {
-    public class UserService : IUserService
+    public class AdminService : IUserService
     {
         private readonly DataContext _dataContext;
-        public UserService(DataContext dataContext)
+        public AdminService(DataContext dataContext)
         {
             _dataContext = dataContext;
         }
         public async Task<User> AddUserAsync(User user)
-        {             
+        {
             var savedUser = _dataContext.Users.Add(user).Entity;
             await _dataContext.SaveChangesAsync();
             return savedUser;
@@ -25,7 +25,14 @@ namespace Pfe.ChatbotApi.Services.Classes
             _dataContext.Users.Remove(user);
             _dataContext.SaveChanges();
             return user;
-        }     
+        }
+
+        public async Task<String> GetUserAsync(int Id)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == Id) ?? throw new Exception("user not found");
+            String result = "Name: " + user.Name + "/n Email:" + user.Email;
+            return result;
+        }
 
         public List<User> List()
         {
@@ -34,10 +41,17 @@ namespace Pfe.ChatbotApi.Services.Classes
 
         public async Task<User> UpdateUserAsync(User user)
         {
-            var dbUser = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == user.Id) ?? throw new Exception("user not found") ;
+            var dbUser = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == user.Id) ?? throw new Exception("user not found");
             _dataContext.Users.Update(user);
             _dataContext.SaveChanges();
             return dbUser;
+        }
+
+        Task<User> IUserService.GetUserAsync(int Id)
+        {
+            var user = _dataContext.Users.FirstOrDefaultAsync(x => x.Id == Id) ?? throw new Exception("user not found");
+
+            return user;
         }
     }
 }
